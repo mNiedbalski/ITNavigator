d52628287b8e17a16c8f8bc208ff1dc73ff1f296
Michał Niedbalski <michnie507@student.polsl.pl>
2025-03-29 17:31:54 +0100

    Testing out darkmode from shadcn/ui

--- it-navigator/src/App.tsx
+++ it-navigator/src/App.tsx
@@ -1,25 +1,28 @@
-import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh w-8/12 mx-auto">
-      <h1 className="text-3xl font-bold mb-6">How to create Cv?</h1>
-      <Card className="w-3/4">
-        <CardHeader>
-          <CardTitle>Step 1</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </CardContent>
-        <CardHeader>
-          <CardTitle>Step 2</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        </CardContent>
-      </Card>
-    </div>
-  )
+    <ThemeProvider>
+      <div className="flex flex-col items-center justify-center min-h-svh w-8/12 mx-auto">
+        <h1 className="text-3xl font-bold mb-6">How to create Cv?</h1>
+        <Card className="w-3/4">
+          <CardHeader>
+            <CardTitle>Step 1</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </CardContent>
+          <CardHeader>
+            <CardTitle>Step 2</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+          </CardContent>
+        </Card>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
